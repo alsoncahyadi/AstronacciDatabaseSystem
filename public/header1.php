3b11d6dd93b45f05399879d98b363aca50ced637
SimpliCty <ndwongso_nikolas@yahoo.com>
2016-12-17 20:50:53 +0700

    Add layout (upload)

---
+++ public/header1.php
@@ -0,0 +1,22 @@
+<div class="row">
+	<div class="col-sm-3">
+		<a href="#"><img src="../ass/logo.png"/></a>
+	</div>
+	<div class="col-sm-5"></div>
+	<div class="col-sm-4">
+		<a href="#"><img src="../ass/logout.png" class="menuicon"/></a>
+		<a href="#"><img src="../ass/setting.png" class="menuicon"/></a>
+		<a href="#"><img src="../ass/notif.png" class="menuicon"/></a>
+		<a href="#"><img src="../ass/profile.png" class="menuicon"/></a>
+  </div>
+</div>
+<nav class="navbar navbar-default" style="opacity:0.8">
+	<div class="container-fluid">
+		<ul class="nav navbar-nav">
+		<li class="active"><a href="#">A-CLUB</a></li>
+		<li><a href="#">CLUB B</a></li>
+		<li><a href="#">3 CLUB</a></li>
+		<li><a href="#">DLL CLUB</a></li>
+		</ul>
+	</div>
+</nav>
