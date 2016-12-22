fc617b4a34d6208a60e5686df30173389497b413
Michael Tjandra <miciltjandra@gmail.com>
2016-12-22 16:08:16 +0700

    Add some validation on import

--- app/Http/Controllers/MRGController.php
+++ app/Http/Controllers/MRGController.php
@@ -18,10 +18,20 @@ class MRGController extends Controller
         //dd($mrgs);
 
         //Judul kolom yang ditampilkan pada tabel
-        $heads = ["Account", "Tanggal Join", "Type", "Sales"];
+        $heads = ["Account", "Tanggal Join", "Type", "Sales", "Birthday", "UOB"];
 
         //Nama attribute pada sql
-        $atts = ["account", "join_date", "type", "sales_username"];
+        $atts = ["account", "join_date", "type", "sales_username", "birthdate", "is_UOB"];
+        foreach ($mrgs as $mrg) {
+            $mrg->is_UOB = $mrg->is_UOB ? "Yes" : "No";
+            $mrg->is_cat = $mrg->is_cat ? "Yes" : "No";
+            $mrg->is_mrg_premiere = $mrg->is_mrg_premiere ? "Yes" : "No";
+            $mrg->is_aclub_stock = $mrg->is_aclub_stock ? "Yes" : "No";
+            $mrg->is_aclub_future = $mrg->is_aclub_future ? "Yes" : "No";
+            foreach ($atts as $att) {
+                if (!$mrg->$att) $mrg->$att = "-";
+            }
+        }
         return view('table\table', ['route' => 'MRG', 'clients' => $mrgs, 'heads'=>$heads, 'atts'=>$atts]);
     }
 
@@ -47,60 +57,80 @@ class MRGController extends Controller
     }
 
     public function importExcel() {
+        $err = [];
         if(Input::hasFile('import_file')){
             $path = Input::file('import_file')->getRealPath();
             $data = Excel::load($path, function($reader) {
             })->get();
             if(!empty($data) && $data->count()){
                 $i = 1;
+                //Cek apakah ada error
                 foreach ($data as $key => $value) {
                     $i++;
                     if (($value->account) === null) {
                         $msg = "Account empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->nama) === null) {
                         $msg = "Nama empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->tanggal_join) === null) {
                         $msg = "Tanggal Join empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->alamat) === null) {
                         $msg = "Alamat empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->kota) === null) {
                         $msg = "Kota empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->telepon) === null) {
                         $msg = "Telepon empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->email) === null) {
                         $msg = "Email empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->type) === null) {
                         $msg = "Type empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
                     if (($value->sales) === null) {
                         $msg = "Sales empty on line ".$i;
-                        return redirect()->back()->withErrors([$msg]);
+                        $err[] = $msg;
                     }
+                } //end validasi
 
-                        echo $value->account . ' ' . $value->nama . ' ' . $value->tanggal_join . ' ' . $value->alamat . ' ' . $value->kota . ' ' . $value->telepon . ' ' . $value->email . ' ' . $value->type . ' ' . $value->sales . ' ' . "<br/>";                
-
-                    DB::select("call inputMRG($value->account,'$value->nama','$value->tanggal_join','$value->alamat','$value->kota','$value->telepon','$value->email','$value->type','$value->sales')");
+                //Jika tidak ada error, import
+                if (empty($err)) {
+                    foreach ($data as $key => $value) {
+                        echo $value->account . ' ' . $value->nama . ' ' . $value->tanggal_join . ' ' . $value->alamat . ' ' . $value->kota . ' ' . $value->telepon . ' ' . $value->email . ' ' . $value->type . ' ' . $value->sales . ' ' . "<br/>";
+                        try { 
+                          DB::select("call inputMRG($value->account,'$value->nama','$value->tanggal_join','$value->alamat','$value->kota','$value->telepon','$value->email','$value->type','$value->sales')");
+                        } catch(\Illuminate\Database\QueryException $ex){ 
+                          echo ($ex->getMessage()); 
+                          $err[] = $ex->getMessage();
+                        }
+                    }
+                    if (empty($err)) { //message jika tidak ada error saat import
+                        $msg = "Excel successfully imported";
+                        $err[] = $msg;
+                    }
                 }
             }
         } else {
             $msg = "No file supplied";
-            return redirect()->back()->withErrors([$msg]);
+            $err[] = $msg;
         }
+
+
+        //foreach ($err as $er) 
+        //    echo $er . "<br/>";
+        return redirect()->back()->withErrors([$err]);
     }
 
     /*public function exportExcel() {
