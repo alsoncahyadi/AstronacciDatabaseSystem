11ede2b4e4c731ea7466b2e09986ee4433a7bc92
Joshua A Kosasih <joshua.kosasih@gmail.com>
2017-11-16 09:57:19 +0700

    update mrg acc seeder

--- database/seeds/MrgAccountTableSeeder.php
+++ database/seeds/MrgAccountTableSeeder.php
@@ -19,12 +19,14 @@ class MrgAccountTableSeeder extends Seeder
     public function makeMagicSeedMrgAccount($amount) {
         for ($i = 0; $i < $amount; $i++) {        
             $magic_seed = new App\MrgAccount;
-            $magic_seed->accounts_number = 999999 - ($amount);
+            $magic_seed->accounts_number = -1 - ($i);
             $magic_seed->master_id = 999999;
             $magic_seed->sales_name = 'MAGIC_SEED_SALES';
             $magic_seed->account_type = 'MAGIC_SEED_ACCOUNT';
             $magic_seed->created_by = 999;
             $magic_seed->updated_by = 999;
+
+            $magic_seed->save();
         }
     }
 }
