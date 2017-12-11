ce7212a33c4faa8fa03d15c16571b8651d9fb5d0
Nikolas Wangsaputra <13514048@std.stei.itb.ac.id>
2017-12-11 15:18:33 +0700

    fix ajax dikit

--- database/seeds/AclubInformationTableSeeder.php
+++ database/seeds/AclubInformationTableSeeder.php
@@ -12,6 +12,17 @@ class AclubInformationTableSeeder extends Seeder
     public function run()
     {
         //
+        $this->makeMagicSeedAclubInformation();
         factory(App\AclubInformation::class, 10)->create();
     }
+
+    public function makeMagicSeedAclubInformation() {
+        $magic_seed = new App\AclubInformation;
+        $magic_seed->master_id = 999999;
+        $magic_seed->sumber_data = '-';
+        $magic_seed->keterangan = '-';
+        $magic_seed->created_by = 999;
+        $magic_seed->updated_by = 999;
+        $magic_seed->save();         
+    }    
 }
--- database/seeds/AclubMemberTableSeeder.php
+++ database/seeds/AclubMemberTableSeeder.php
@@ -12,6 +12,17 @@ class AclubMemberTableSeeder extends Seeder
     public function run()
     {
         //
+        $this->makeMagicSeedAclubMember();
         factory(App\AclubMember::class, 10)->create();
+    }
+
+    public function makeMagicSeedAclubMember() {
+        $magic_seed = new App\AclubMember;
+        $magic_seed->user_id = 999999;
+        $magic_seed->master_id = 999999;
+        $magic_seed->group = 'S';
+        $magic_seed->created_by = 999;
+        $magic_seed->updated_by = 999;
+        $magic_seed->save();
     }
 }
