63420317a07e243bfa91fe4c3cd97bb85a0129f0
Joshua A Kosasih <joshua.kosasih@gmail.com>
2018-01-25 13:38:08 +0700

    fix bug

--- resources/views/vpc/uobtable.blade.php
+++ resources/views/vpc/uobtable.blade.php
@@ -7,18 +7,19 @@
                                         <input class="selectable" id="{{ $client->client_id }}" onchange="" type="checkbox" style="" name="assigned{{ $idx }}">
                                         <input type="hidden" name="id{{ $idx }}" value="">
                                     @foreach ($attsMaster as $attMaster)
-                                        @if ($attMaster == 'name')
+{{--                                        @if ($attMaster == 'name')
                                         <td class="fixed-side" style="white-space: nowrap; pointer-events: auto !important;"> <a id="{{$attMaster}}_{{$client->user_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;"> {{ $client->$attMaster }}</a></td>
-                                        @else
+                                        @else --}}
                                         <td class="fixed-side" onclick="copyFunction(this)" style="white-space: nowrap; cursor:context-menu;"> {{ $client->$attMaster }} </td>
-                                        @endif
+                                        {{--@endif--}}
+                                        
                                     @endforeach
                                     @foreach ($atts as $att)
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD
                                         <td onclick="copyFunction(this)" style="max-width: 100px; white-space: nowrap; cursor:context-menu;"> {{$client->$att}} </td>
-=======
+======= -->
                                         <td style="max-width: 100px; white-space: nowrap;"> <a id="{{$att}}_{{$client->client_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;">{{$client->$att}} </a></td>
->>>>>>> 131fac5ac3ac9e7c7e6f9bfa7a2b73f8dc713c59
+<!-- >>>>>>> 131fac5ac3ac9e7c7e6f9bfa7a2b73f8dc713c59 -->
                                     @endforeach
                                 </tr>
                             <?php $idx = $idx + 1; ?>
