6f299bdcd29a88a61e2a304fcf12ccb786d26874
alvinjunianto <alvinjunianto@gmail.com>
2018-01-25 12:24:39 +0700

    UOB styling

--- resources/views/vpc/uobtable.blade.php
+++ resources/views/vpc/uobtable.blade.php
@@ -6,10 +6,14 @@
                                         <input class="selectable" id="{{ $client->user_id }}" onchange="" type="checkbox" style="" name="assigned{{ $idx }}">
                                         <input type="hidden" name="id{{ $idx }}" value="">
                                     @foreach ($attsMaster as $attMaster)
-                                        <td class="fixed-side" style="white-space: nowrap;"> {{ $client->$attMaster }}</td>
+                                        @if ($attMaster == 'name')
+                                        <td class="fixed-side" style="white-space: nowrap; pointer-events: auto !important;"> <a id="{{$attMaster}}_{{$client->user_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;"> {{ $client->$attMaster }}</a></td>
+                                        @else
+                                        <td class="fixed-side" onclick="copyFunction(this)" style="white-space: nowrap; cursor:context-menu;"> {{ $client->$attMaster }} </td>
+                                        @endif
                                     @endforeach
                                     @foreach ($atts as $att)
-                                        <td style="max-width: 100px; white-space: nowrap;"> <a id="{{$att}}_{{$client->user_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;">{{$client->$att}} </a></td>
+                                        <td onclick="copyFunction(this)" style="max-width: 100px; white-space: nowrap; cursor:context-menu;"> {{$client->$att}} </td>
                                     @endforeach
                                 </tr>
                             <?php $idx = $idx + 1; ?>
