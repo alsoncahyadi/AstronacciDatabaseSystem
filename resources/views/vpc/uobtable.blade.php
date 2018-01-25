43ab8acfdba2bbffe0e0c86590336078c9658a58
alvinjunianto <alvinjunianto@gmail.com>
2018-01-25 12:34:54 +0700

    add some styling

--- resources/views/vpc/uobtable.blade.php
+++ resources/views/vpc/uobtable.blade.php
@@ -10,17 +10,12 @@
                                         @if ($attMaster == 'name')
                                         <td class="fixed-side" style="white-space: nowrap; pointer-events: auto !important;"> <a id="{{$attMaster}}_{{$client->user_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;"> {{ $client->$attMaster }}</a></td>
                                         @else
-                                        <td class="fixed-side" onclick="copyFunction(this)" style="white-space: nowrap; cursor:context-menu;"> {{ $client->$attMaster }} </td>
+                                        <td class="fixed-side" onclick="copyFunction(this)" style="white-space: nowrap; cursor:context-menu;"> ok {{ $client->$attMaster }} </td>
                                         @endif
                                     @endforeach
                                     @foreach ($atts as $att)
-<<<<<<< HEAD
-                                        <td onclick="copyFunction(this)" style="max-width: 100px; white-space: nowrap; cursor:context-menu;"> {{$client->$att}} </td>
-=======
-                                        <td style="max-width: 100px; white-space: nowrap;"> <a id="{{$att}}_{{$client->client_id}}" target="_blank" href="{{route($route . '.detail', ['id' => $client->master_id])}}" style="text-decoration:none; color:black;">{{$client->$att}} </a></td>
->>>>>>> 131fac5ac3ac9e7c7e6f9bfa7a2b73f8dc713c59
+                                        <td onclick="copyFunction(this)" style="max-width: 100px; white-space: nowrap; cursor:context-menu;"> ok1 {{$client->$att}} </td>
                                     @endforeach
                                 </tr>
                             <?php $idx = $idx + 1; ?>
                             @endforeach
-                        
