ca2cdd491ae14d614aeb5cd2c96bb46a5846beed
SimpliCty <nikolas.wangsaputra@gmail.com>
2018-03-20 18:13:49 +0700

    add provinsi-kota bind dropdown

--- resources/views/content/addclient.blade.php
+++ resources/views/content/addclient.blade.php
@@ -46,6 +46,14 @@
 							<input class="form-control no-spin" type="date" name="{{strtolower(str_replace(' ', '_', $atr))}}">
 					<?php elseif (($str_id == 'NoHP') || ($str_id == 'NoTelepon')) : ?>
 							<input class="form-control no-spin" type="number" name="{{strtolower(str_replace(' ', '_', $atr))}}">
+					<?php elseif ($str_id == 'Provinsi') : ?>
+							<select class="form-control" name="{{strtolower(str_replace(' ', '_', $atr))}}" id="prov">
+
+							</select>
+					<?php elseif ($str_id == 'Kota') : ?>
+							<select class="form-control" name="{{strtolower(str_replace(' ', '_', $atr))}}" id="kota">
+
+							</select>
 					<?php else : ?>
 							<input class="form-control" type="text" name="{{strtolower(str_replace(' ', '_', $atr))}}">
 					<?php endif; ?>
@@ -415,6 +423,30 @@
 	});
     // ======================================================================================================
     
+    var prov = {
+    	"Aceh" : ["Banda Aceh", "Langsa", "Lhokseumawe", "Meulaboh", "Sabang", "Subulussalam"],
+    	"Bali" : ["Denpasar"],
+        "Bangka Belitung" : ["Pangkalpinang"],
+    };
+    var provKeys = Object.keys(prov);
+	var select = document.getElementById('prov');
+	var selectKota = document.getElementById('kota');
+	for(var i=0; i< provKeys.length; i++)
+	{
+	  select.options[i] = new Option(provKeys[i], provKeys[i]);  //new Option("Text", "Value")
+	}
+	for(var i=0; i< prov["Aceh"].length; i++)
+	{
+	  selectKota.options[i] = new Option(prov["Aceh"][i], prov["Aceh"][i]);  //new Option("Text", "Value")
+	}
+	$('#prov').on('change', function() {
+	  $("#kota").empty();
+	  var temp = this.value;
+	  for(var i=0; i< prov[temp].length; i++)
+		{
+		  selectKota.options[i] = new Option(prov[temp][i], prov[temp][i]);  //new Option("Text", "Value")
+		}
+	})
 </script>
 
 @endsection
