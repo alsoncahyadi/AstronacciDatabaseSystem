050ff5085814599ffd5287deeaa20aab31f93ce7
Jovian <christiantojovian@gmail.com>
2018-03-21 08:25:27 +0700

    fixed province city edit client

--- resources/views/profile/pcdetail.blade.php
+++ resources/views/profile/pcdetail.blade.php
@@ -108,10 +108,12 @@
                                         <option>F</option>
                                     </select>
                                 @elseif ($key == 'Provinsi')
+                                    <input type="hidden" name="prov" id="currentProv" value="{{$client_master->$value}}">
                                     <select class="form-control" name="{{$value}}" id="prov" value="{{$client_master->$value}}">
 
                                     </select>
                                 @elseif ($key == 'Kota') 
+                                    <input type="hidden" name="kota" id="currentCity" value="{{$client_master->$value}}">
                                     <select class="form-control" name="{{$value}}" id="kota" value="{{$client_master->$value}}">
 
                                     </select>
@@ -644,22 +646,56 @@
     }
     // ======================================================================================================
 
-    /*var prov = {
+    var prov = {
         "Aceh" : ["Banda Aceh", "Langsa", "Lhokseumawe", "Meulaboh", "Sabang", "Subulussalam"],
         "Bali" : ["Denpasar"],
         "Bangka Belitung" : ["Pangkalpinang"],
+        "Banten" : ["Cilegon", "Serang", "Tangerang Selatan", "Tangerang"],
+        "Bengkulu" : ["Bengkulu"],
+        "Gorontalo" : ["Gorontalo"],
+        "Jakarta" : ["Jakarta Barat", "Jakarta Pusat", "Jakarta Selatan", "Jakarta Timur", "Jakarta Utara"],
+        "Jambi" : ["Sungai Penuh", "Jambi"],
+        "Jawa Barat" : ["Bandung", "Bekasi", "Bogor", "Cimahi", "Cirebon", "Depok", "Sukabumi", "Tasikmalaya", "Banjar"],
+        "Jawa Tengah" : ["Magelang", "Pekalongan", "Purwokerto", "Salatiga", "Semarang", "Surakarta", "Tegal"],
+        "Jawa Timur" : ["Batu", "Blitar", "Kediri", "Madiun", "Malang", "Mojokerto", "Pasuruan", "Probolinggo", "Surabaya"],
+        "Kalimantan Barat" : ["Pontianak", "Singkawang"],
+        "Kalimantan Selatan" : ["Banjarbaru", "Banjarmasin"],
+        "Kalimantan Tengah" : ["Palangkaraya"],
+        "Kalimatan Timur" : ["Balikpapan", "Bontang", "Samarinda"],
+        "Kalimantan Utara" : ["Tarakan"],
+        "Kepulauan Riau" : ["Batam", "Tanjungpinang"],
+        "Lampung" : ["Bandar Lampung", "Metro"],
+        "Maluku Utara" : ["Ternate", "Kepulauan Tidore"],
+        "Maluku" : ["Ambon", "Tual"],
+        "Nusa Tenggara Barat" : ["Bima", "Mataram"],
+        "Nusa Tenggara Timur" : ["Kupang"],
+        "Papua Barat" : ["Sorong"],
+        "Papua" : ["Jayapura"],
+        "Riau" : ["Dumai", "Pekanbaru"],
+        "Sulawesi Selatan" : ["Makassar", "Palopo", "Parepare"],
+        "Sulawesi Tengah" : ["Palu"],
+        "Sulawesi Tenggara" : ["Bau-Bau", "Kendari"],
+        "Sulawesi Utara" : ["Bitung", "Kotamobagu", "Manado", "Tomohon"],
+        "Sumatera Barat" : ["Bukittinggi", "Padang", "Padangpanjang", "Pariaman", "Payakumbuh", "Sawahlunto", "Solok"],
+        "Sumatera Selatan" : ["Lubuklinggau", "Pagaralam", "Palembang", "Prabumulih"],
+        "Sumatera Utara" : ["Binjai", "Medan", "Padang Sidempuan", "Pematangsiantar", "Sibolga", "Tanjungbalai", "Tebingtinggi"],
+        "Yogyakarta" : ["Yogyakarta"],
     };
     var provKeys = Object.keys(prov);
     var select = document.getElementById('prov');
     var selectKota = document.getElementById('kota');
+    var currentProv = document.getElementById('currentProv').value;
+    var currentCity = document.getElementById('currentCity').value;
     for(var i=0; i< provKeys.length; i++)
     {
       select.options[i] = new Option(provKeys[i], provKeys[i]);  //new Option("Text", "Value")
     }
-    for(var i=0; i< prov["Aceh"].length; i++)
+    for(var i=0; i< prov[currentProv].length; i++)
     {
-      selectKota.options[i] = new Option(prov["Aceh"][i], prov["Aceh"][i]);  //new Option("Text", "Value")
+      selectKota.options[i] = new Option(prov[currentProv][i], prov[currentProv][i]);  //new Option("Text", "Value")
     }
+    document.getElementById("prov").value = currentProv;
+    document.getElementById("kota").value = currentCity;
     $('#prov').on('change', function() {
       $("#kota").empty();
       var temp = this.value;
@@ -667,7 +703,7 @@
         {
           selectKota.options[i] = new Option(prov[temp][i], prov[temp][i]);  //new Option("Text", "Value")
         }
-    })*/
+    })
 	</script>
 </body>
 </html>
